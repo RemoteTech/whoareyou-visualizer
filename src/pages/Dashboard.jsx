80903facbb1a89ca7d8494937ace44bf54797b75
Juan Velez <jvelez@remotetech.nyc>
2025-05-08 22:26:46 -0400

    Code update

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,3 +1,4 @@
+import SignalsReport from '../components/SignalsReport';
 import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import LikesChart from '../components/LikesChart';
